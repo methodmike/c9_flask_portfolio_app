9de3ebc31c95e88489c593bb73cf74f2a3bf5658
methodmike <mike.schmitt@protonmail.com>
2018-04-17 15:09:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new_name = name + ' likes to eat mangos'
-	return render_template('index.html', new_name)
+	new_name = str(name) + ' likes to eat mangos'
+	return render_template('index.html', name= new_name)
 
 
 @app.route('/add_numbers', methods=['GET','POST'])
